f7a8927cbda95a40aae1d1a0bf583f4d9c19b8cf
Pavel Vasilev <pavelabadon@gmail.com>
2025-09-16 15:04:19 +0300

    add dinamic dogs from dogs.js

---
+++ src/dogs.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    "id": 1,
+    "name": "Rex",
+    "breed": "German Shepherd",
+    "description": "Loyal and protective, great working dog and family companion.",
+    "imageUrl": "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg"
+  },
+  {
+    "id": 2,
+    "name": "Bella",
+    "breed": "Golden Retriever",
+    "description": "Friendly, intelligent and devoted. Loves playing fetch.",
+    "imageUrl": "https://images.pexels.com/photos/4587990/pexels-photo-4587990.jpeg"
+  },
+  {
+    "id": 3,
+    "name": "Max",
+    "breed": "Labrador Retriever",
+    "description": "Gentle and loving, enjoys swimming and long walks.",
+    "imageUrl": "https://images.pexels.com/photos/4588016/pexels-photo-4588016.jpeg"
+  },
+  {
+    "id": 4,
+    "name": "Lucy",
+    "breed": "Beagle",
+    "description": "Curious and merry, follows scents everywhere.",
+    "imageUrl": "https://images.pexels.com/photos/4587998/pexels-photo-4587998.jpeg"
+  },
+  {
+    "id": 5,
+    "name": "Charlie",
+    "breed": "French Bulldog",
+    "description": "Adaptable and playful, loves cuddles on the sofa.",
+    "imageUrl": "https://images.pexels.com/photos/4588052/pexels-photo-4588052.jpeg"
+  },
+  {
+    "id": 6,
+    "name": "Milo",
+    "breed": "Husky",
+    "description": "Energetic and vocal, with striking blue eyes.",
+    "imageUrl": "https://images.pexels.com/photos/4587973/pexels-photo-4587973.jpeg"
+  },
+  {
+    "id": 7,
+    "name": "Daisy",
+    "breed": "Cocker Spaniel",
+    "description": "Affectionate, cheerful, and always wagging her tail.",
+    "imageUrl": "https://images.pexels.com/photos/4588030/pexels-photo-4588030.jpeg"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,52 +1,67 @@
 import http from 'http';
 import fs from 'fs/promises';
+import dogs from './dogs.js'; // твоя JSON масив с кучета
 
-const server = http.createServer(async (req, res) =>{
-    if (req.url === '/'){
-        const homeHtml = await fs.readFile('./src/views/home/index.html', {encoding: 'utf-8'});
-        res.writeHead(200, {
-        "content-type": 'text/html'
-    });
+const server = http.createServer(async (req, res) => {
+    try {
+        // Главна страница с кучета
+        if (req.url === '/') {
+            const homeHtml = await fs.readFile('./src/views/home/index.html', 'utf-8');
+            const dogsHtml = dogs.map(dog => dogTemplate(dog)).join('\n');
+            const result = homeHtml.replace('{{dogs}}', dogsHtml);
 
-    res.write(homeHtml);
-    res.end();
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(result);
 
-    }else if(req.url === '/cats/add-breed'){
-        const addBreadHtml = await fs.readFile('./src/views/addBreed.html', {encoding: 'utf-8'});
-        res.writeHead(200, {
-        "content-type": 'text/html'
-    });
+        // Добавяне на порода
+        } else if (req.url === '/dogs/add-breed') {
+            const addBreedHtml = await fs.readFile('./src/views/addBreed.html', 'utf-8');
 
-    res.write(addBreadHtml);
-    res.end();
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(addBreedHtml);
 
-    }else if(req.url === '/cats/add-cat'){
-        const addCatHtml = await fs.readFile('./src/views/addCat.html', {encoding: 'utf-8'});
-        res.writeHead(200, {
-        "content-type": 'text/html'
-    });
+        // Добавяне на куче
+        } else if (req.url === '/dogs/add-dog') {
+            const addDogHtml = await fs.readFile('./src/views/addDog.html', 'utf-8');
 
-    res.write(addCatHtml);
-    res.end();
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(addDogHtml);
 
+        // CSS файл
+        } else if (req.url === '/styles/site.css') {
+            const homeCss = await fs.readFile('./src/content/styles/site.css', 'utf-8');
+
+            res.writeHead(200, { "content-type": "text/css" });
+            res.end(homeCss);
+
+        // Всички други пътища
+        } else {
+            res.writeHead(404, { "content-type": "text/plain" });
+            res.end("404 Not Found");
+        }
+
+    } catch (err) {
+        res.writeHead(500, { "content-type": "text/plain" });
+        res.end("Server error: " + err.message);
     }
-    else if(req.url === '/styles/site.css'){
-        const homeCss = await fs.readFile('./src/content/styles/site.css', {encoding: 'utf-8'});
-        res.writeHead(200,{
-            "content-type": 'text/css'
-        });
-
-        res.write(homeCss);
-
-    res.end()
-
-    }else{
-        res.end();
-    }
-    
-    
 });
 
-server.listen(5000);
+// Функция за генериране на HTML за едно куче
+function dogTemplate(dog) {
+    return `
+        <li>
+            <img src="${dog.imageUrl}" alt="${dog.name}">
+            <h3>Name: ${dog.name}</h3>
+            <p><span>Breed: </span>${dog.breed}</p>
+            <p><span>Description: </span>${dog.description}</p>
+            <ul class="buttons">
+                <li class="btn edit"><a href="">Change Info</a></li>
+                <li class="btn delete"><a href="">New Home</a></li>
+            </ul>
+        </li>
+    `;
+}
 
-console.log('Server is listening on http://localhost:5000...');
+server.listen(5000, () => {
+    console.log('Server is listening on http://localhost:5000...');
+});
